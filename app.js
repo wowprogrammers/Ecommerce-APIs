91d0b6975aaceec6a5f98b5810a6725b060f598c
wowprogrammers <wowprogrammersfounder@gmail.com>
2023-10-25 00:34:20 +0500

    Implemented Logging Functionality

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 const app = express();
 import dotenv from 'dotenv'
+import path from 'path';
+import morgan from 'morgan';
+import fs from 'fs';
 dotenv.config();
-import './Db/conn.js'; 
+import './Db/conn.js';  
 app.use(express.json());
 
+// Create a write streams
+let accesslogStream = fs.createWriteStream(path.join('./access.log'),{flags:'a'});
+
+// Set Up the logger
+
+app.use(morgan('combined', {stream:accesslogStream})); 
+app.use(morgan(':response-time ms',{stream:accesslogStream}))
 
 // Importing routes for mouning 
 import {userRoute} from './routes/userRoute.js' 
